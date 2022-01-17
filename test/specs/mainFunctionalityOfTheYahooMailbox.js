34686fa165be297373ac30a70a260b98a0acd1b4
Ivan <80200619+Ivan643@users.noreply.github.com>
2022-01-17 11:17:08 +0300

    Update mainFunctionalityOfTheYahooMailbox.js

--- test/specs/mainFunctionalityOfTheYahooMailbox.js
+++ test/specs/mainFunctionalityOfTheYahooMailbox.js
@@ -4,88 +4,65 @@ describe('Verify main functionality of the mailbox', function() {
     var subjectText = 'The email testing!';
     var bodyText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.';
 
-  it('should login to the mailbox',async function() {
+  it('should login to the mailbox', async function() {
     await browser.url ("https://www.yahoo.com/");
-    var loginButton = await $('._yb_k2zgk');
-    await loginButton.click();
-    var inputEmailField = await $('#login-username');
-    await inputEmailField.click();
+    await $('._yb_k2zgk').click();
+    await $('#login-username').click();
     await browser.keys('testmailbox123123@yahoo.com'); 
-    var nextButton = await $('#login-signin');
-    await nextButton.click();
-    var inputPasswordField = $('#login-passwd');
-    await inputPasswordField.click();
+    await $('#login-signin').click();
+    await $('#login-passwd').click();
     await browser.keys('automatedTestingIsAwesome');
     await nextButton.click();
   });
 
-  it('should verify that the login was successfull',async function() {
-    var labelOfProfile = $('#ybarAccountMenuOpener');
-    await expect(labelOfProfile).toExist();
+  it('should verify that the login was successfull', async function() {
+    await expect($('#ybarAccountMenuOpener')).toExist();
   });
 
-  it('should create a new email and should save it to the drafts folder',async function() {
-    var mailButton = await $('#ybarMailLink');
-    await mailButton.click();
-    var composeButton = await $('a[href*="/d/compose/"]');
-    await composeButton.click();
-    var inputAddressField = await $('#message-to-field');
-    await inputAddressField.click();
+  it('should create a new email and should save it to the drafts folder', async function() {
+    var mailButton = ;
+    await $('#ybarMailLink').click();
+    await $('a[href*="/d/compose/"]').click();
+    await $('#message-to-field').click();
     await browser.keys(addressText);
-    var inputSubjectField = await $('input[data-test-id*="compose-subject"]');
-    await inputSubjectField.click();
+    await $('input[data-test-id*="compose-subject"]').click();
     await browser.keys(subjectText);
-    var inputBodyField = await $('#editor-container');
-    await inputBodyField.click();
+    await $('#editor-container').click();
     await browser.keys(bodyText);
-    var draftsFolderButton = await $('li div[data-test-folder-container*="Draft"]');
-    await draftsFolderButton.click();
+    await $('li div[data-test-folder-container*="Draft"]').click();
   });
 
-  it('should verify that the email presents in the drafts folder',async function() {
-    var draftEmail = await $('a[aria-label*="From: test1234512345@yopmail.com"]');
-    await expect(draftEmail).toExist();
+  it('should verify that the email presents in the drafts folder', async function() {
+    await expect($('a[aria-label*="From: test1234512345@yopmail.com"]')).toExist();
   });
 
-  it('should verify the draft content',async function() {
-    var draftEmail = await $('li a[aria-label*="From: test1234512345@yopmail.com"]');
-    await draftEmail.click();
-    var emailAddress = await $('.P_oRhLS');
-    await expect(emailAddress).toHaveTextContaining(addressText);
-    var subject = await $('input[data-test-id*="compose-subject"]');
-    await expect(subject).toHaveValueContaining(subjectText);
-    var body = await $('div[dir*="ltr"]');
-    await expect(body).toHaveTextContaining(bodyText);
+  it('should verify the draft content', async function() {
+    await $('li a[aria-label*="From: test1234512345@yopmail.com"]').click();
+    await expect($('.P_oRhLS')).toHaveTextContaining(addressText);
+    await expect($('input[data-test-id*="compose-subject"]')).toHaveValueContaining(subjectText);
+    await expect($('div[dir*="ltr"]')).toHaveTextContaining(bodyText);
   });
 
-  it('should send the email',async function() {
-    var sendButton = await $('button[data-test-id*="compose-send-button"]');
-    await sendButton.click();
+  it('should send the email', async function() {
+    await $('button[data-test-id*="compose-send-button"]').click();
   });
 
-  it('should verify that the email has removed from the drafts folder',async function() {
-    var draftEmail = await $('a[aria-label*="From: test1234512345@yopmail.com"]');
-    await expect(draftEmail).toExist();
+  it('should verify that the email has removed from the drafts folder', async function() {
+    await expect($('a[aria-label*="From: test1234512345@yopmail.com"]')).toExist();
   });
 
-  it('should verify that the email is in Sent folder',async function() {
-    var sentFolderButton = await $('li div[data-test-folder-container*="Draft"]');
-    await sentFolderButton.click();
-    var sentEmail = await $('a[aria-label*="From: test1234512345@yopmail.com"]');
-    await expect(sentEmail).toExist();
+  it('should verify that the email is in Sent folder', async function() {
+    await $('li div[data-test-folder-container*="Draft"]').click();
+    await expect($('a[aria-label*="From: test1234512345@yopmail.com"]')).toExist();
   });
 
-  it('should logout from the account',async function() {
-  var profileItem = await $('#ybarAccountMenu');
-  await profileItem.moveTo();
-  var logoutButton = await $('a#profile-signout-link');
-  await logoutButton.waitForDisplayed();
-  await logoutButton.click();
+  it('should logout from the account', async function() {
+  await $('#ybarAccountMenu').moveTo();
+  await $('a#profile-signout-link').waitForDisplayed();
+  await $('a#profile-signout-link').click();
   });
 
-  it('should verify that the logout was successfull',async function() {
-    var loginButton = await $('._yb_k2zgk');
-    await expect(loginButton).toExist();
+  it('should verify that the logout was successfull', async function() {
+    await expect($('._yb_k2zgk')).toExist();
   });
-  
 });

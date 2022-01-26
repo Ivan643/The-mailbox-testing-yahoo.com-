36ce7bfde15a2d615233073716c7ad7797f7d6df
Ivan <80200619+Ivan643@users.noreply.github.com>
2022-01-26 19:21:20 +0300

    Update start.page.js

--- test/pages/start.page.js
+++ test/pages/start.page.js
@@ -1,13 +1,13 @@
-/* eslint-disable no-undef */
-/* eslint-disable linebreak-style */
-/* eslint-disable class-methods-use-this */
-
 const Page = require('./page');
+
+const selectors = {
+  loginButton: './/*[@id="ybar-inner-wrap"]/div[3]/div/div[3]/div[1]/div/a'
+};
 
 class StartPage extends Page {
   async open() { super.open('https://www.yahoo.com/'); }
 
-  get loginButton() { return $('.//*[@id="ybar-inner-wrap"]/div[3]/div/div[3]/div[1]/div/a'); }
+  get loginButton() { return $(selectors.loginButton); }
 }
 
 module.exports = new StartPage();

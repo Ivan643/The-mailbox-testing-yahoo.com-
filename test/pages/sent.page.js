1c822fe961f3eae02016711ad320d67e18f3d738
Ivan <80200619+Ivan643@users.noreply.github.com>
2022-01-26 19:20:54 +0300

    Update sent.page.js

--- test/pages/sent.page.js
+++ test/pages/sent.page.js
@@ -1,15 +1,17 @@
-/* eslint-disable no-undef */
-/* eslint-disable linebreak-style */
-/* eslint-disable class-methods-use-this */
-
 const Page = require('./page');
 
+const selectors = {
+  sentEmail: 'a[aria-label*="From: test1234512345@yopmail.com"]',
+  profileItem: '#ybarAccountMenu',
+  logoutButton: '//*[@id="profile-signout-link"]'
+};
+
 class SentPage extends Page {
-  get sentEmail() { return $('a[aria-label*="From: test1234512345@yopmail.com"]'); }
+  get sentEmail() { return $(selectors.sentEmail); }
 
-  get profileItem() { return $('#ybarAccountMenu'); }
+  get profileItem() { return $(selectors.profileItem); }
 
-  get logoutButton() { return $('//*[@id="profile-signout-link"]'); }
+  get logoutButton() { return $(selectors.logoutButton); }
 
   async logout() { await this.logoutButton.waitForDisplayed(); this.logoutButton.click(); }
 }

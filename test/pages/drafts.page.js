f04d7a28a64f6cced9736f509ff6aebe6f8460cd
Ivan <80200619+Ivan643@users.noreply.github.com>
2022-01-26 19:17:05 +0300

    Update drafts.page.js

--- test/pages/drafts.page.js
+++ test/pages/drafts.page.js
@@ -1,15 +1,17 @@
-/* eslint-disable no-undef */
-/* eslint-disable linebreak-style */
-/* eslint-disable class-methods-use-this */
-
 const Page = require('./page');
 
+const selectors = {
+  draftEmail: 'a[aria-label*="From: test1234512345@yopmail.com"]',
+  draftEmailList: '//*[@id="mail-app-component"]/div/div/div[2]/div/div/div[2]/div/div[1]/ul/li[2]',
+  sentFolderButton: 'li div[data-test-folder-container*="Draft"]'
+};
+
 class DraftsPage extends Page {
-  get draftEmail() { return $('a[aria-label*="From: test1234512345@yopmail.com"]'); }
+  get draftEmail() { return $(selectors.draftEmail); }
 
-  get draftEmailList() { return $('//*[@id="mail-app-component"]/div/div/div[2]/div/div/div[2]/div/div[1]/ul/li[2]'); }
+  get draftEmailList() { return $(selectors.draftEmailList); }
 
-  get sentFolderButton() { return $('li div[data-test-folder-container*="Draft"]'); }
+  get sentFolderButton() { return $(selectors.sentFolderButton); }
 }
 
 module.exports = new DraftsPage();

19415d3aac4105431c5c89281b19d8058a19f183
Ivan <80200619+Ivan643@users.noreply.github.com>
2022-02-05 20:14:29 +0300

    Update drafts.steps.js

--- features/step-definitions/drafts.steps.js
+++ features/step-definitions/drafts.steps.js
@@ -1,19 +1,19 @@
-const { Given, When, Then } = require('@wdio/cucumber-framework');
-
-const expectChai = require('chai').expect;
-
+const { When, Then } = require('@wdio/cucumber-framework');
+const { expect } = require('chai');
 const draftsPage = require('../pageobjects/drafts.page');
 
 Then(/^I should see the created draft in the Drafts folder$/, async () => {
-  await expect(draftsPage.draftEmailList).toExist();
+  await draftsPage.draftEmailList.waitForDisplayed();
+  expect(await draftsPage.draftEmailList.isDisplayed()).is.equal(true); 
 });
 
-When(/^I click on the created draft$/, async () => {
+When(/^I open the created draft$/, async () => {
   await draftsPage.draftEmailList.click();
 });
 
 Then(/^I should see that the draft has removed from the Drafts folder$/, async () => {
-  expectChai(await draftsPage.emptyDraftFolder.isDisplayed()).to.be.equal(true);
+  await draftsPage.draftEmailList.waitForDisplayed({reverse: true});
+  expect(await draftsPage.draftEmailList.isDisplayed()).is.equal(false);
 });
 
 When(/^I click the Sent folder button$/, async () => {

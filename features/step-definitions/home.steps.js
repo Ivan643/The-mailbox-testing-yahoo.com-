3f5e6945ac27f363aece60b5fcbc5e40291047c1
Ivan <80200619+Ivan643@users.noreply.github.com>
2022-02-05 20:15:23 +0300

    Update home.steps.js

--- features/step-definitions/home.steps.js
+++ features/step-definitions/home.steps.js
@@ -1,10 +1,10 @@
-const { Given, When, Then } = require('@wdio/cucumber-framework');
-
+const { When, Then } = require('@wdio/cucumber-framework');
+const { expect } = require('chai');
 const homePage = require('../pageobjects/home.page');
 
 Then(/^I should see a Home page$/, async () => {
-    await expect(await homePage.labelOfProfile).toExist();
-  });
+  expect(await homePage.labelOfProfile.isDisplayed()).is.equal(true);
+});
 
 When(/^I click the Mail button$/, async () => {
   await homePage.mailButton.click();

09bd537e218cd5f4aaba48ab62f3929de8d59783
Ivan <80200619+Ivan643@users.noreply.github.com>
2022-02-05 20:13:38 +0300

    Update compose.steps.js

--- features/step-definitions/compose.steps.js
+++ features/step-definitions/compose.steps.js
@@ -1,25 +1,25 @@
-const { Given, When, Then } = require('@wdio/cucumber-framework');
-
+const { When, Then } = require('@wdio/cucumber-framework');
+const { expect } = require('chai');
 const composePage = require('../pageobjects/compose.page');
 
 Then(/^I should see a Compose page with Address, Subject and Body fields$/, async () => {
-  await expect(composePage.inputAddressField).toExist();
-  await expect(composePage.inputSubjectField).toExist();
-  await expect(composePage.inputBodyField).toExist();
+  expect(await composePage.inputAddressField.isDisplayed()).is.equal(true);
+  expect(await composePage.inputSubjectField.isDisplayed()).is.equal(true);
+  expect(await composePage.inputBodyField.isDisplayed()).is.equal(true);
 });
-  
-When(/^I fill the Address field with 'test1234512345@yopmail.com'$/, async () => {
-  await composePage.inputAddressField.setValue('test1234512345@yopmail.com');
+
+When(/^I fill the Address field with '(.*)'$/, async (emailAddressText) => {
+  await composePage.inputAddressField.setValue(emailAddressText);
 });
-  
-When(/^I fill the Subject field with 'The email testing!'$/, async () => {
-  await composePage.inputSubjectField.setValue('The email testing!');
+
+When(/^I fill the Subject field with '(.*)'$/, async (subjectText) => {
+  await composePage.inputSubjectField.setValue(subjectText);
 });
-  
-When(/^I fill the Body field with 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'$/, async () => {
-  await composePage.inputBodyField.setValue('Lorem ipsum dolor sit amet, consectetur adipiscing elit.');
+
+When(/^I fill the Body field with '(.*)'$/, async (bodyText) => {
+  await composePage.inputBodyField.setValue(bodyText);
 });
-  
+
 When(/^I click the Drafts folder button$/, async () => {
   await composePage.draftsFolderButton.click();
 });

2155c9c679384b41993e25bd60ce701ff97e5a5a
Ivan <80200619+Ivan643@users.noreply.github.com>
2022-02-07 12:33:48 +0300

    Update login.steps.js

--- features/step-definitions/login.steps.js
+++ features/step-definitions/login.steps.js
@@ -1,21 +1,20 @@
-const { Given, When, Then } = require('@wdio/cucumber-framework');
-
-const expectChai = require('chai').expect;
-
+const { When, Then } = require('@wdio/cucumber-framework');
+const { expect } = require('chai');
 const loginPage = require('../pageobjects/login.page');
 
-When(/^I enter the mailbox name 'testmailbox123123@yahoo.com'$/, async () => {
-  await loginPage.inputEmailField.setValue('testmailbox123123@yahoo.com');
-  expectChai(await loginPage.inputEmailField.getValue()).to.be.equal('testmailbox123123@yahoo.com');
+When(/^I enter the mailbox name '(.*)'$/, async (mailboxName) => {
+  await loginPage.inputEmailField.setValue(mailboxName);
+  expect(await loginPage.inputEmailField.getValue()).is.equal(mailboxName);
   await loginPage.nextButton.click();
 });
   
 Then(/^I should see a Login page with a password field$/, async () => {
-  await expect(await loginPage.inputPasswordField).toExist();
+  await loginPage.inputPasswordField.waitForDisplayed();
+  expect(await loginPage.inputPasswordField.isDisplayed()).is.equal(true);
 });
   
-When(/^I enter the password 'automatedTestingIsAwesome'$/, async () => {
-  await loginPage.inputPasswordField.setValue('automatedTestingIsAwesome');
-  expectChai(await loginPage.inputPasswordField.getValue()).to.be.equal('automatedTestingIsAwesome');
+When(/^I enter the password '(.*)'$/, async (password) => {
+  await loginPage.inputPasswordField.setValue(password);
+  expect(await loginPage.inputPasswordField.getValue()).is.equal(password);
   await loginPage.nextButton.click();
 });
